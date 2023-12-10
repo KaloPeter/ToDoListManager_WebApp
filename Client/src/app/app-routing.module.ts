708661f642f65ca66f7bb0980e4308000cb199e0
kalopeter8 <kalopeter8@gmail.com>
2023-12-10 11:32:49 +0100

    RoutingForMoreEfficientContentAccesDisplay

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { ToDoComponent } from './Components/to-do/to-do.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'todo', component: ToDoComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' }//"**" means not in the list
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
