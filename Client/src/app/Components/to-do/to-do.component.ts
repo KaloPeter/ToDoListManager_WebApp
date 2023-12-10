c0fa876a8bac345ad6304f9a6113765f34fd3145
kalopeter8 <kalopeter8@gmail.com>
2023-12-10 11:29:58 +0100

    ToCreateEventOnAChoosenDate

---
+++ Client/src/app/Components/to-do/to-do.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do',
+  templateUrl: './to-do.component.html',
+  styleUrls: ['./to-do.component.css']
+})
+export class ToDoComponent {
+
+}
