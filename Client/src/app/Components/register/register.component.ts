1c25d05dcf0110930f3d3eecc9772958a039232a
kalopeter8 <kalopeter8@gmail.com>
2023-12-23 11:37:59 +0100

    UserRegistrationForAccessingAddEventFeature

--- Client/src/app/Components/register/register.component.ts
+++ Client/src/app/Components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/_Services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+
+  registerModel: any = {};
+
+  constructor(private accountService: AccountService, private router: Router) { }
+
+
+  register() {
+    this.accountService.register(this.registerModel).subscribe(
+      {
+        next: () => this.router.navigateByUrl("/"),
+        error: er => console.log("Error during Register: " + er)
+      }
+    )
+  }
+
 
 }
