45cdce9caceb7eb5bdf36d133ed3d086b6895468
kalopeter8 <kalopeter8@gmail.com>
2023-12-23 10:34:52 +0100

    HttpClientComponentImports

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +17,8 @@ import { AlertModule } from 'ngx-bootstrap/alert';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RatingModule } from 'ngx-bootstrap/rating';
 import { ToDoEventMapComponent } from './Components/to-do-event-map/to-do-event-map.component';
+import { JwtInterceptor } from './_Interceptors/jwt';
+import { ToDoEventCardComponent } from './Components/to-do-event-card/to-do-event-card.component';
 
 
 @NgModule({
@@ -25,19 +29,24 @@ import { ToDoEventMapComponent } from './Components/to-do-event-map/to-do-event-
     RegisterComponent,
     NavComponent,
     ToDoComponent,
-    ToDoEventMapComponent
+    ToDoEventMapComponent,
+    ToDoEventCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
+    BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
     AlertModule.forRoot(),
     TabsModule.forRoot(),
     RatingModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
