3b1fec5e0f2c947877eae57eeadf4788b20c1d5d
kalopeter8 <kalopeter8@gmail.com>
2023-12-23 10:29:57 +0100

    ContentDisplayBasedOnLoggedUserExistence

--- Client/src/app/Components/nav/nav.component.ts
+++ Client/src/app/Components/nav/nav.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserToken } from 'src/app/_Models/userToken';
+import { AccountService } from 'src/app/_Services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  loggedUserObs: Observable<UserToken | null> | null = null;
+  constructor(private accountService: AccountService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loggedUserObs = this.accountService.currentUser$
+  }
+
+
+
+  signIn() {
+    this.router.navigateByUrl('/login');
+  }
+
+  signUp() {
+    this.router.navigateByUrl('/register');
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
 
 }
